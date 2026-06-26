036e2a3a2e434382a4a342e0716b5ed5b4f833d4
Seig Desamito <hecthor212005@gmail.com>
2026-06-26 09:53:03 +0800

    Update master_server_group12.py

--- master_server_group12.py
+++ master_server_group12.py
@@ -1,59 +1,60 @@
-
 import Pyro5.api
+import Pyro5.config
 import json
-import os
+import threading
 from datetime import datetime
+
+Pyro5.config.SERVERTYPE = "thread"
 
 @Pyro5.api.expose
 class TaskQueue:
 
     def __init__(self):
-        self.tasks = {}
+        self.lock = threading.Lock()
 
         try:
             with open("tasks.json", "r") as file:
                 self.tasks = json.load(file)
 
-                for task in self.tasks.values():
-                    task["status"] = "pending"
-                    task["result"] = None
+            for task in self.tasks.values():
+                task["status"] = "pending"
+                task["result"] = None
 
             print("Tasks loaded successfully.")
 
         except FileNotFoundError:
-            print("Error: tasks.json not found.")
+            print("tasks.json not found")
             self.tasks = {}
 
     def request_task(self):
+        with self.lock:
+            for task_id, task in self.tasks.items():
+                if task["status"] == "pending":
+                    task["status"] = "in-progress"
 
-        for task_id, task in self.tasks.items():
+                    print(f"Task {task_id} assigned.")
 
-            if task["status"] == "pending":
-
-                task["status"] = "in-progress"
-
-                print(f"Task {task_id} assigned.")
-
-                return task_id, task
+                    return task_id, task
 
         return None
 
     def submit_result(self, task_id, result, worker_name):
+        with self.lock:
+            if task_id in self.tasks:
+                self.tasks[task_id]["status"] = "completed"
+                self.tasks[task_id]["result"] = result
 
-        if task_id in self.tasks:
+                timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-            self.tasks[task_id]["status"] = "completed"
-            self.tasks[task_id]["result"] = result
+                print(f"[{timestamp}] [SUCCESS] Task {task_id} completed by {worker_name}. Result: {result}")
 
-            timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-            print(f"[{timestamp}] [SUCCESS] Task {task_id} completed by {worker_name}. Result: {result}")
-
-daemon = Pyro5.server.Daemon()
+daemon = Pyro5.api.Daemon()
 
 ns = Pyro5.api.locate_ns()
 
-uri = daemon.register(TaskQueue)
+queue = TaskQueue()
+
+uri = daemon.register(queue)
 
 ns.register("MasterServer", uri)
 
